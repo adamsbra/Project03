f09e03f07221b77b214d044724705fe7c6388095
Brandon Adams <adamsb02@moravian.edu>
2019-02-26 19:13:48 -0500

    Update AddressGenerator.java

--- src/AddressGenerator.java
+++ src/AddressGenerator.java
@@ -6,25 +6,37 @@ import java.util.Random;
 
 public class AddressGenerator {
 
+    //Define filename
     private String filename = "random_addresses.txt";
+    //Just for storing south and east.
     private String[] directions = {"South", "East"};
 
     public String createRandomAddress(){
+        //Get a Random instance
         Random rand = new Random();
+        //Instantiate house number
         int house_number = 0;
+        //Weird comparison but it works, wont accept numbers with 00 in tens place
         while (house_number % 100 == 0|| house_number % 10 != 0){
+            //Random numbers between 1 and 2000
             house_number = rand.nextInt(2000);
         }
+        //Prints out the house number, street type, and street number.
+        //Street number is random number 1 through 20
         return house_number + " " + directions[rand.nextInt(2)] + " " + rand.nextInt(20) + "\n";
 
     }
 
     public void generateAddresses() throws IOException {
+        //Opens a new file
         File file = new File(filename);
+        //Creates a buffered writer
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         for (int i = 0; i < 100; i++){
+            //Writes a random address 100 times.
             writer.write(createRandomAddress());
         }
+        //Close writer
         writer.close();
     }
 }
