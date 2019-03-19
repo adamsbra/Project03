7b70a295956dc5ef6726adeeede0c5255d2073d1
Brandon Adams <adamsb02@moravian.edu>
2019-03-19 11:24:10 -0400

    Changed to using military time.

--- src/AddressGenerator.java
+++ src/AddressGenerator.java
@@ -12,34 +12,17 @@ public class AddressGenerator {
     private String filename = "random_addresses.txt";
     //Just for storing south and east.
     private String[] directions = {"South", "East"};
-    private int current_hour = 10;
-    private int current_minute = 0;
+    private LocalTime time = LocalTime.of(7, 0, 0);
 
     private String createRandomAddress(){
         //Get a Random instance
         Random rand = new Random();
         //Instantiate house number
         int house_number = 0;
-        int new_hour = 0;
-        String time_of_day = "AM";
         int rand_minutes = rand.nextInt(10);
-        if (current_minute + rand_minutes >= 60){
-            current_hour += 1;
-            current_minute = current_minute + rand_minutes - 60;
-        }
-        else{
-            current_minute = current_minute + rand_minutes;
-        }
-        if (current_hour > 12){
-            new_hour = current_hour - 12;
-            time_of_day = "PM";
-        }
-        else if (current_hour == 12){
-            new_hour = current_hour;
-            time_of_day = "PM";
-        }
-        else{
-            new_hour = current_hour;
+        time = time.plusMinutes(rand_minutes);
+        if (time.getHour() >= 18) {
+            time = LocalTime.of(18, 0, 0);
         }
         //Weird comparison but it works, wont accept numbers with 00 in tens place
         while (house_number % 100 == 0|| house_number % 10 != 0){
@@ -52,10 +35,8 @@ public class AddressGenerator {
 
         //Prints out the house number, street type, and street number.
         //Street number is random number 1 through 20
-        if (current_minute < 10){
-            return house_number + " " + direction + " " + street_number + " " + new_hour + ":0" + current_minute + time_of_day +  "\n";
-        }
-        return house_number + " " + direction + " " + street_number + " " + new_hour + ":" + current_minute  + time_of_day + "\n";
+        return house_number + " " + direction + " " + street_number + " " + time.toString() +  "\n";
+
 
     }
 
